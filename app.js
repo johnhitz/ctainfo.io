97d4a38332d9c1368897344a7bcae2430f2b7292
John Hitz <johnh@whodapet.com>
2020-02-08 23:18:16 -0600

    Placing stop names for route in html

--- app.js
+++ app.js
@@ -8,17 +8,28 @@ $(() => {
   // let args = '&rt=3&format=json'
   // const url = `http://www.ctabustracker.com/bustime/api/v2/${unique}?key=ySBXDn8J2axRZgjHsVfp3CquD${args}`
   const url = `http://ctabustracker.com/bustime/api/v2/${unique}?key=ySBXDn8J2axRZgjHsVfp3CquD&rt=${args}`
+  getStops(3, 'Northbound')
+
+})
+
+const getStops = (route, direction) => {
+  const unique = "getstops"
+  const bustimeResponse = 'bustime-response'
+  let args = `&rt=${route}&dir=${direction}&format=json`
   $.ajax(
     {
-      // url: 'http://ctabustracker.com/bustime/api/v2/getvehicles?key=ySBXDn8J2axRZgjHsVfp3CquD&vid=6438,1295&tmres=s&format=json',
-      url: url,
+      url: `http://ctabustracker.com/bustime/api/v2/${unique}?key=ySBXDn8J2axRZgjHsVfp3CquD&rt=${args}`,
       success: (data) => {
-        console.log(data);
-         // console.log(data['bustime-response'].vehicle[0].vid);
+        console.log(data['bustime-response'].stops[0].stpnm);
+        const length = data['bustime-response'].stops.length
+        // console.log(Object.keys(data));
+        for(let i = 0; i < length; i++) {
+          const stopName = data['bustime-response'].stops[i].stpnm
+          $card = $(`<div class='card'><h3>Name: ${stopName}</h3</div>`)
+          $('.stops').append($card)
+        }
+
       },
-      // headers: {"X-My-Custom-Header": "some value"}
     }
   )
-})
-// rt=20&dir=East%20Bound&format=json
-// &rt=20&dir=East%20Bound&format=json
+};
