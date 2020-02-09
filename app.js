196c50fcda2455932ec83bccab7129f118c4ab6f
John Hitz <johnh@whodapet.com>
2020-02-08 19:50:38 -0600

    Passing url as a variable

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 console.log($);
 $(() => {
+  const url = `http://www.ctabustracker.com/bustime/api/v2/getroutes?key=ySBXDn8J2axRZgjHsVfp3CquD&rt=2&format=json`
   $.ajax(
     {
       // url: 'http://ctabustracker.com/bustime/api/v2/getvehicles?key=ySBXDn8J2axRZgjHsVfp3CquD&vid=6438,1295&tmres=s&format=json',
-      url: 'http://www.ctabustracker.com/bustime/api/v2/getroutes?key=ySBXDn8J2axRZgjHsVfp3CquD&rt=2&format=json',
+      url: url,
       success: (data) => {
         console.log(data);
          // console.log(data['bustime-response'].vehicle[0].vid);
