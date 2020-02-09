94091898f5fd1758a43bd4eb056b3d7cc5440184
John Hitz <johnh@whodapet.com>
2020-02-08 19:54:30 -0600

    passing unique segment and args into string

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 console.log($);
 $(() => {
-  const url = `http://www.ctabustracker.com/bustime/api/v2/getroutes?key=ySBXDn8J2axRZgjHsVfp3CquD&rt=2&format=json`
+  let unique = "getroutes"
+  let args = '&rt=2&format=json'
+  const url = `http://www.ctabustracker.com/bustime/api/v2/${unique}?key=ySBXDn8J2axRZgjHsVfp3CquD${args}`
   $.ajax(
     {
       // url: 'http://ctabustracker.com/bustime/api/v2/getvehicles?key=ySBXDn8J2axRZgjHsVfp3CquD&vid=6438,1295&tmres=s&format=json',
