0609fa2fad53997a9d4ea259088498cf9ae5a085
John Hitz <johnh@whodapet.com>
2020-02-08 22:03:25 -0600

    fixed problem with getstops url and passing string  segments unique and args to for unique url for fetching data

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 console.log($);
 $(() => {
-  let unique = "getroutes"
-  let args = '&rt=2&format=json'
-  const url = `http://www.ctabustracker.com/bustime/api/v2/${unique}?key=ySBXDn8J2axRZgjHsVfp3CquD${args}`
+  // let unique = "getroutes"
+  // let args = '&rt=2&format=json'
+  let unique = "getstops"
+  let args = '&rt=3&dir=Northbound&format=json'
+  // let unique = 'getroutes'
+  // let args = '&rt=3&format=json'
+  // const url = `http://www.ctabustracker.com/bustime/api/v2/${unique}?key=ySBXDn8J2axRZgjHsVfp3CquD${args}`
+  const url = `http://ctabustracker.com/bustime/api/v2/${unique}?key=ySBXDn8J2axRZgjHsVfp3CquD&rt=${args}`
   $.ajax(
     {
       // url: 'http://ctabustracker.com/bustime/api/v2/getvehicles?key=ySBXDn8J2axRZgjHsVfp3CquD&vid=6438,1295&tmres=s&format=json',
@@ -15,3 +20,5 @@ $(() => {
     }
   )
 })
+// rt=20&dir=East%20Bound&format=json
+// &rt=20&dir=East%20Bound&format=json
