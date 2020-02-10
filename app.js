18d4b833a8732b787a53654802145e23be115e26
John Hitz <johnh@whodapet.com>
2020-02-09 22:35:33 -0600

    Moved url and API key to top level variables, added comments for getstops function, and added list of data I need to finish app

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 $(() => {
+  // Base url to be used in all request to the API
+  const url = 'http://ctabustracker.com/bustime/api/v2/'
+  // API key
+  const key = 'ySBXDn8J2axRZgjHsVfp3CquD'
+  //////////////////////////////////////////
+  // Routes: unique = getstops,
+  // args = `&rt=${route}&dir=${direction}&format=json`
+  // Get stop information for a given Route.
+  // Takes an alphanumeric designator of the route (required) and
+  // the direction (eg Northbound, required) and returns
+  // [name,stop id, longitude, latitude, ...] for all
+  // stops on the given route
   $('.button').on('click', (event) => {
     const unique = "getstops"
     const bustimeResponse = 'bustime-response'
     let route = $('input[id="route-input"]').val()
     let direction = $('input[id="direction-input"]').val()
     let args = `&rt=${route}&dir=${direction}&format=json`
+
+    //clear fields
+    route = $('input[id="route-input"]').val("")
+direction = $('input[id="direction-input"]').val("")
     $.ajax(
       {
-        url: `http://ctabustracker.com/bustime/api/v2/${unique}?key=ySBXDn8J2axRZgjHsVfp3CquD&rt=${args}`,
+        url: url + `${unique}?key=${key}&rt=${args}`,
         success: (data) => {
           const length = data['bustime-response'].stops.length
+          console.log(data['bustime-response'].stops[0]);
           for(let i = 0; i < length; i++) {
             const stopName = data['bustime-response'].stops[i].stpnm
             const stopId = data['bustime-response'].stops[i].stpid
@@ -23,4 +40,15 @@ $(() => {
       }
     )
   })
+
+  //////////////////////////////////////////
+  // Routes: unique = getroutes, args = null
+  //////////////////////////////////////////
+  // Directions: unique = getdirections ,
+  // args = &rt=${route}&stpid=${stopId}&format=json
+  //////////////////////////////////////////
+  // Predictions: unique = getpredictions, args = &rt=${route}&format=json
+  //////////////////////////////////////////
+
+
 })
